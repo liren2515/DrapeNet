3fe43c49146c60c611ac21d5d39312f5f70e4d06
Kristijan <kristijan.bartol%40gmail.com>
2023-07-13 13:06:24 +0200

    Update git.sh to ask for name and email.

--- git.sh
+++ git.sh
@@ -1,4 +1,13 @@
 #!/bin/bash/
 
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
+
+echo -e "\nEnter your name and email to configure Git"
+read -p "Your name:" name
+read -p "Email:" email
+name=$(urle $name)
+email=$(urle $email)
+
+git config --global user.name $name
+git config --global user.email $email 
+
